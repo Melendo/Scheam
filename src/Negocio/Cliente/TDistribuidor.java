227d5896c3df07e152c2790cb570c1ba628b8e8b
rulohdez <rauher05@ucm.es>
2023-04-26 09:35:15 +0200

    TCliente TDistribuidor TParticular

--- src/Negocio/Cliente/TDistribuidor.java
+++ src/Negocio/Cliente/TDistribuidor.java
@@ -3,43 +3,26 @@
  */
 package Negocio.Cliente;
 
-/** 
- * <!-- begin-UML-doc -->
- * <!-- end-UML-doc -->
- * @author 34601
- * @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
- */
 public class TDistribuidor extends TCliente {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	private String CIF;
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	private String direccion;
+	
 	public String getCIF() {
 		return CIF;
 	}
+	
 	public void setCIF(String cIF) {
-		CIF = cIF;
+		this.CIF = cIF;
 	}
+	
 	public String getDireccion() {
 		return direccion;
 	}
+	
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	
 }
