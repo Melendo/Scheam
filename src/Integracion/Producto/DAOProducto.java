cdc61389f05ecf5a0eeb02f305198e6e91521a6a
jorge <jserra07@ucm.es>
2023-04-26 10:04:54 +0200

    DAOs Productos sin comentarios

--- src/Integracion/Producto/DAOProducto.java
+++ src/Integracion/Producto/DAOProducto.java
@@ -1,23 +1,10 @@
-/**
- * 
- */
 package Integracion.Producto;
 
 import Negocio.Producto.TProducto;
 import java.util.Set;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author 34601
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class DAOProducto implements IDAOProducto {
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#create(TProducto producto)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public Integer create(TProducto producto) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -25,11 +12,6 @@ public class DAOProducto implements IDAOProducto {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#delete(Integer idproducto)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer delete(Integer idproducto) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -37,11 +19,6 @@ public class DAOProducto implements IDAOProducto {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#modify(TProducto producto)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer modify(TProducto producto) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -49,11 +26,6 @@ public class DAOProducto implements IDAOProducto {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#readAll()
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Set readAll() {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -61,11 +33,6 @@ public class DAOProducto implements IDAOProducto {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#readById(Integer idproducto)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TProducto readById(Integer idproducto) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -73,11 +40,6 @@ public class DAOProducto implements IDAOProducto {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOProducto#cerrarProducto(Integer idproducto)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer cerrarProducto(Integer idproducto) {
 		// begin-user-code
 		// TODO Auto-generated method stub
--- src/Integracion/Producto/IDAOProducto.java
+++ src/Integracion/Producto/IDAOProducto.java
@@ -1,68 +1,14 @@
-/**
- * 
- */
 package Integracion.Producto;
 
 import Negocio.Producto.TProducto;
 import java.util.Set;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author 34601
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public interface IDAOProducto {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param producto
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public Integer create(TProducto producto);
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idproducto
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer delete(Integer idproducto);
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param producto
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer modify(TProducto producto);
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Set readAll();
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idproducto
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public TProducto readById(Integer idproducto);
-
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @param idproducto
-	* @return
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer cerrarProducto(Integer idproducto);
 }
