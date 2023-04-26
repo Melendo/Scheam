c07aff9ff1bd7dfad6bb6fb11156d7eb356580b3
jorge <jserra07@ucm.es>
2023-04-26 19:31:53 +0200

    SAProducto -> altaProducto

--- src/Negocio/Producto/SAProducto.java
+++ src/Negocio/Producto/SAProducto.java
@@ -2,13 +2,29 @@ package Negocio.Producto;
 
 import java.util.Set;
 
+import Integracion.Factorias.FactoriaDAOImp;
+
 public class SAProducto implements ISAProducto {
 
 	public Integer altaProducto(TProducto producto) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		System.out.println("Intentando altaProducto - SAProducto");
+		TProducto emp = FactoriaDAOImp.getInstance().getDaoProducto().readById(producto.getIdproyecto());
+
+		if (emp.getIdproyecto() == -1) {
+			System.out.println("altaProducto Realizado (creado) - SAProducto");
+			return FactoriaDAOImp.getInstance().getDaoProducto().create(producto);
+		} else {
+			if (emp.getActivo()) {
+				System.out.println("altaProducto No Realizado (existe y activo) - SAProducto");
+				return -1;
+			} else {
+				producto.setActivo(true);
+				producto.setIdproyecto(emp.getIdproyecto());
+				System.out.println("altaProducto Realizado (reactivado) - SAProducto");
+				FactoriaDAOImp.getInstance().getDaoProducto().modify(producto);
+				return 2;
+			}
+		}
 	}
 
 	public Integer bajaProducto(Integer IDProducto) {
