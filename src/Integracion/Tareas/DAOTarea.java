fb821600293bb661bd0c6835e7ec5344266a40ea
Adrizz08 <adriro11@ucm.es>
2023-04-27 20:02:28 +0200

    DAOTarea create

--- src/Integracion/Tareas/DAOTarea.java
+++ src/Integracion/Tareas/DAOTarea.java
@@ -1,36 +1,56 @@
-/**
- * 
- */
 package Integracion.Tareas;
 
 import Negocio.Tareas.TTarea;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Set;
 import Negocio.Producto.TProducto;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author 34601
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class DAOTarea implements IDAOTarea {
-	/** 
-	* (non-Javadoc)
-	* @see IDAOTarea#create(TTarea tarea)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
+	Connection con;
+	
+	public DAOTarea() {
+	
+	System.out.println("Intentando Conexión - DAOTarea");
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/scheam", "root", "");
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	System.out.println("Conexión Realizada - DAOTarea");
+
+	}
+	
 	public Integer create(TTarea tarea) {
-		// begin-user-code
-		// TODO Auto-generated method stub
-		return null;
-		// end-user-code
+		
+		System.out.println("Intentando create - DAOTarea");
+		try {
+			PreparedStatement ps;
+			String sql = "INSERT INTO tarea (nombre, equipo, producto, terminada, activo) VALUES (?,?,?,?,?);";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, tarea.getNombre());
+			ps.setInt(2, tarea.getEquipo());
+			ps.setInt(3, tarea.getProducto());
+			ps.setBoolean(4, tarea.getTerminada());
+			ps.setBoolean(5, true);
+
+			ps.executeUpdate();
+			con.close();
+			System.out.println("Create Realizado - DAOTarea");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 1;
+
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see IDAOTarea#delete(Integer idtarea)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public Integer delete(Integer idtarea) {
 		// begin-user-code
 		// TODO Auto-generated method stub
