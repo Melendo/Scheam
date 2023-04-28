40a1d313ba59b85aeddb5954ccc831bd2c2e8d07
rulohdez <rauher05@ucm.es>
2023-04-28 20:05:48 +0200

    MainWindowCliente

--- src/Presentacion/Cliente/MainWindowCliente.java
+++ src/Presentacion/Cliente/MainWindowCliente.java
@@ -6,20 +6,12 @@ package Presentacion.Cliente;
 import javax.swing.JFrame;
 import java.awt.event.ActionListener;
 import Presentacion.IGUI;
+import Presentacion.Controlador.Eventos;
+
 import java.awt.event.ActionEvent;
 
-/** 
-* <!-- begin-UML-doc -->
-* <!-- end-UML-doc -->
-* @author 34601
-* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-*/
 public class MainWindowCliente extends JFrame implements ActionListener, IGUI {
-	/** 
-	* <!-- begin-UML-doc -->
-	* <!-- end-UML-doc -->
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
+
 	public void vMainWindowCliente() {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -27,11 +19,6 @@ public class MainWindowCliente extends JFrame implements ActionListener, IGUI {
 		// end-user-code
 	}
 
-	/** 
-	* (non-Javadoc)
-	* @see ActionListener#actionPerformed(ActionEvent e)
-	* @generated "UML a Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
-	*/
 	public void actionPerformed(ActionEvent e) {
 		// begin-user-code
 		// TODO Auto-generated method stub
@@ -39,11 +26,12 @@ public class MainWindowCliente extends JFrame implements ActionListener, IGUI {
 		// end-user-code
 	}
 
-
-
 	@Override
 	public void update(int event, Object object) {
-		// TODO Auto-generated method stub
-		
+		switch(event) {
+		case Eventos.MainWindowEmpleado:
+			setVisible(true);
+			break;
+		}		
 	}
 }
