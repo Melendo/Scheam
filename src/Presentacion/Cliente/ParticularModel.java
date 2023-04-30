8c5b83f40a6014ee2d0542f31dd411cd259995b9
rulohdez <rauher05@ucm.es>
2023-04-30 23:28:07 +0200

    particular model

--- src/Presentacion/Cliente/ParticularModel.java
+++ src/Presentacion/Cliente/ParticularModel.java
@@ -1,5 +1,7 @@
 package Presentacion.Cliente;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.swing.table.AbstractTableModel;
@@ -8,27 +10,49 @@ import Negocio.Cliente.TParticular;
 
 public class ParticularModel extends AbstractTableModel{
 
+	String[] header = {"ID", "Nombre", "Email", "DNI", "telefono"};
+	List<TParticular> particulares;
+	
+	public ParticularModel() {
+		particulares = new ArrayList<TParticular>();
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		return header[column];
+	}
+
 	@Override
 	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return particulares.size();
 	}
 
 	@Override
 	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return header.length;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		switch (columnIndex) {
+		case 0:
+			return particulares.get(rowIndex).getID();
+		case 1:
+			return particulares.get(rowIndex).getNombre();
+		case 2:
+			return particulares.get(rowIndex).getEmail();
+		case 3:
+			return particulares.get(rowIndex).getDNI();
+		case 4:
+			return particulares.get(rowIndex).getTelefono();
+		default:
+			return null;
+		}
 	}
 
 	public void setLista(Set<TParticular> lista) {
-		
-		
+		if (lista != null) 
+			particulares.addAll(lista);
 	}
 
 }
