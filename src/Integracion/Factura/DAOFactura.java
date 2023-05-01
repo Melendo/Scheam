7365302c5eeea74bb3a1ed6012e9fd967cfef528
Melendo <100785500+Melendo@users.noreply.github.com>
2023-05-01 11:20:14 +0200

    DAOFactura

--- src/Integracion/Factura/DAOFactura.java
+++ src/Integracion/Factura/DAOFactura.java
@@ -4,9 +4,13 @@
 package Integracion.Factura;
 
 import java.sql.Connection;
+import java.sql.Date;
+
 import Negocio.Factura.TFactura;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Set;
 
 public class DAOFactura implements IDAOFactura {
@@ -25,8 +29,25 @@ public class DAOFactura implements IDAOFactura {
 	}
 
 	public Integer create(TFactura factura) {
-		
-		return null;
+		System.out.println("Intentando create - DAOFactura");
+		try {
+			PreparedStatement ps;
+			String sql = "INSERT INTO factura (idcliente, fecha, importe, activo) VALUES (?,?,?,?);";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, factura.getIDCliente());
+			ps.setDate(2, (Date) factura.getFecha());
+			ps.setDouble(3, factura.getImporte());
+			ps.setBoolean(4, true);
+
+			ps.executeUpdate();
+			con.close();
+			System.out.println("Create Realizado tabla factura- DAOFactura");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		return 1;
 	}
 
 	public Integer delete(Integer idfactura) {
