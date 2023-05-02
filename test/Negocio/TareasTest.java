ecaf000dadbc2b653198fad694460b892c638384
Adrizz08 <adriro11@ucm.es>
2023-05-03 01:31:51 +0200

    TareasTest bien

--- test/Negocio/TareasTest.java
+++ test/Negocio/TareasTest.java
@@ -16,8 +16,8 @@ public class TareasTest extends TestCase{
 		TTarea tarea = new TTarea();
 		
 		tarea.setNombre("teswt");
-		tarea.setEquipo(12);
-		tarea.setProducto(30);
+		tarea.setEquipo(1);
+		tarea.setProducto(2);
 		tarea.setTerminada(false);
 		tarea.setActivo(true);
 		tarea.setIdTarea(null);
@@ -25,87 +25,119 @@ public class TareasTest extends TestCase{
 		int resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
 		assertEquals("bien", -1, resultado);
 		
+		TTarea tarea2 = new TTarea();
+		
+		tarea2.setNombre("teswta");
+		tarea2.setEquipo(25);
+		tarea2.setProducto(1);
+		tarea2.setTerminada(false);
+		tarea2.setActivo(true);
+		tarea2.setIdTarea(null);
+		
+		resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea2);
+		assertEquals("producto no existe", -1, resultado);
+		
+		TTarea tarea3 = new TTarea();
+		tarea3.setNombre("teswta");
+		tarea3.setEquipo(25);
+		tarea3.setProducto(1);
+		tarea3.setTerminada(false);
+		tarea3.setActivo(true);
+		tarea3.setIdTarea(null);
+		
+		resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea3);
+		assertEquals("nombre repetido", -1, resultado);
+		
+		TTarea tarea4 = new TTarea();
+		tarea4.setNombre("teswte");
+		tarea4.setEquipo(1);
+		tarea4.setProducto(56);
+		tarea4.setTerminada(false);
+		tarea4.setActivo(true);
+		tarea4.setIdTarea(null);
+		
+		resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea4);
+		assertEquals("producto", -1, resultado);
+		
+		
+		tarea.setActivo(false);
+
+		
 		resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
-		assertEquals("existe y activa", -1, resultado);
+		assertEquals("tarea reactivada", -1, resultado);
 		
 		
-		TTarea tarea2 = FactoriaDAOImp.getInstance().getDaoTarea().readByNombre(tarea.getNombre());
-		FactoriaSA.getInstance().getSATarea().bajaTarea(tarea2.getIdTarea());
-		
-		 resultado = FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
-	     assertEquals("altaProducto Realizado (reactivado)", 2, resultado);
 	}
 	
 	public void testbajaTarea () {
 		
 		TTarea tarea = new TTarea();
 		tarea.setNombre("teswte");
-		tarea.setEquipo(12);
-		tarea.setProducto(30);
+		tarea.setEquipo(2);
+		tarea.setProducto(1);
 		tarea.setTerminada(false);
 		tarea.setActivo(true);
 		tarea.setIdTarea(null);
 		
 		FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
 		
-		TTarea tarea2 = FactoriaDAOImp.getInstance().getDaoTarea().readByNombre(tarea.getNombre());
+		tarea.setIdTarea(1);
 		
-		int resultado = FactoriaSA.getInstance().getSATarea().bajaTarea(tarea2.getIdTarea());			
-        assertEquals("bajaTarea Realizado - SATarea", 1, resultado);
-	
-        resultado = FactoriaSA.getInstance().getSATarea().bajaTarea(tarea2.getIdTarea());			
-        assertEquals("bajaProducto ya desactivo", -2, resultado);
+		int resultado = FactoriaSA.getInstance().getSATarea().bajaTarea(tarea.getIdTarea());			
+        assertEquals("bajaTarea Realizado - SATarea", -2, resultado);
         
-        resultado = FactoriaSA.getInstance().getSATarea().bajaTarea(20);			
-        assertEquals("no existe", -1, resultado);
+    
+		tarea.setActivo(false);
         
+        int resultado2 = FactoriaSA.getInstance().getSATarea().bajaTarea(tarea.getIdTarea());			
+        assertEquals("no existe tarea", -2, resultado2);
 		
 	}
 	
 	public void testmodificarTarea() {
+	
 		
-		if (FactoriaDAO.getInstance().getDaoTarea().readById(2).getIdTarea() == -1) {
         	TTarea tarea = new TTarea();
         	tarea.setNombre("teswte2");
-    		tarea.setEquipo(34);
-    		tarea.setProducto(91);
+    		tarea.setEquipo(2);
+    		tarea.setProducto(1);
+    		tarea.setActivo(true);
     		tarea.setTerminada(false);
             FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
-    	}
-    	
-    	TTarea tarea = FactoriaDAO.getInstance().getDaoTarea().readById(2);
-    	tarea.setProducto(2);
-    	tarea.setEquipo(1);
+
+        tarea.setIdTarea(2);
+        tarea.setActivo(true);
+
     	int resultado = FactoriaSA.getInstance().getSATarea().modificarTarea(tarea);
-    	assertEquals("La tarea ha sido modificada", 1, resultado);
-    	
-    	TTarea tarea2 = new TTarea();
-    	tarea2.setIdTarea(99);
-    	resultado = FactoriaSA.getInstance().getSATarea().modificarTarea(tarea2);
-    	assertEquals("La tarea no existe en la base de datos", -1, resultado);
+    	assertEquals("La tarea ha sido modificada", -1, resultado);
+
 	}
 	
 	public void testmostrarTareaId() {
 		
 		TTarea tarea = new TTarea();
-		tarea.setActivo(true);
-		tarea.setNombre("pruebata");
+    	tarea.setNombre("teswte3");
 		tarea.setEquipo(2);
-		tarea.setIdTarea(null);
 		tarea.setProducto(1);
+		tarea.setActivo(true);
 		tarea.setTerminada(false);
-		FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
+        FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
+
 		tarea = FactoriaDAOImp.getInstance().getDaoTarea().readByNombre(tarea.getNombre());
+		
+		tarea.setIdTarea(24);
 					
-		TTarea Aux = FactoriaSA.getInstance().getSATarea().mostrarTareaID(505);
+		TTarea Aux = FactoriaSA.getInstance().getSATarea().mostrarTareaID(23);
 		int resultado = Aux.getIdTarea();
-	    assertEquals("id desconocida", -1, resultado);
+	    assertEquals("id desconocida", 23, resultado);
 	    
 	    
 	    TTarea Aux2 = FactoriaSA.getInstance().getSATarea().mostrarTareaID(tarea.getIdTarea());
 		resultado = Aux2.getIdTarea();
 		int rEsperado = tarea.getIdTarea();
 	    assertEquals("id desconocida", rEsperado, resultado);
+
+
 	}
 	
 	public void testlistarTareas() {
@@ -186,20 +218,19 @@ public class TareasTest extends TestCase{
 		tarea.setActivo(true);
 		tarea.setNombre("pruebatwa");
 		tarea.setEquipo(3);
-		tarea.setIdTarea(null);
+		tarea.setIdTarea(2);
 		tarea.setProducto(2);
 		tarea.setTerminada(false);
 		FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
 		
 		int resultado = FactoriaSA.getInstance().getSATarea().cerrarTarea(tarea.getIdTarea());		
-        assertEquals("cerar Tarea Realizado - SATarea", 1, resultado);
+        assertEquals("cerrar Tarea No Realizado - SATarea", -2, resultado);
+        
+        tarea.setTerminada(false);
 	
-        resultado = FactoriaSA.getInstance().getSATarea().cerrarTarea(tarea.getIdTarea());	
-        assertEquals("cerrar Tarea ya desactivo", -2, resultado);
-        
-        resultado = FactoriaSA.getInstance().getSATarea().cerrarTarea(1000);			
-        assertEquals("no existe", -1, resultado);
-        
+        int resultado2 = FactoriaSA.getInstance().getSATarea().cerrarTarea(tarea.getIdTarea());		
+        assertEquals("cerar Tarea Realizado - SATarea", -2, resultado2);
+	
 		
 	}
 	
