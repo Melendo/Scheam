83bd22e3d91db80b0decaca6ac7d5fe4eb0fd794
rulohdez <rauher05@ucm.es>
2023-05-02 23:14:43 +0200

    ClienteAltaTest

--- test/Negocio/ClienteTest.java
+++ test/Negocio/ClienteTest.java
@@ -5,10 +5,9 @@ import Negocio.Cliente.TCliente;
 import Negocio.Cliente.TDistribuidor;
 import Negocio.Cliente.TParticular;
 import Negocio.Factorias.FactoriaSA;
-
 import junit.framework.TestCase;
 
-public class ClienteTest {
+public class ClienteTest extends TestCase {
 
 	public void testAltaCliente() {
 		// Cliente Distribuidor
@@ -55,56 +54,5 @@ public class ClienteTest {
 		assertEquals("El cliente NO se ha dado de alta. Ya existe", -1, resultadoAPar3);
 
 	}
-
-	public void testBajaProducto() {
-
-		/*
-		TProducto producto = new TProducto();
-		producto.setActivo(true);
-		producto.setFechalanzamiento(17102003);
-		producto.setGenero("Aventura");
-		producto.setIdproyecto(null);
-		producto.setNombre("Pakimone");
-		producto.setPEGI(18);
-		producto.setPrecio(15.98);
-		producto.setStock(3);
-		producto.setTerminado(false);
-
-		FactoriaSA.getInstance().getSAProducto().altaProducto(producto);
-
-		TProducto productoAux = FactoriaDAOImp.getInstance().getDaoProducto().readByNombre(producto.getNombre());
-
-		int resultado = FactoriaSA.getInstance().getSAProducto().bajaProducto(productoAux.getIdproyecto());
-		assertEquals("bajaProducto Realizado - SAProducto", 1, resultado);
-
-		resultado = FactoriaSA.getInstance().getSAProducto().bajaProducto(productoAux.getIdproyecto());
-		assertEquals("bajaProducto ya desactivo", -2, resultado);
-
-		resultado = FactoriaSA.getInstance().getSAProducto().bajaProducto(10000);
-		assertEquals("no existe", -1, resultado);
-
-		FactoriaSA.getInstance().getSAProducto().altaProducto(producto);
-
-		TEquipo equipo = new TEquipo();
-		equipo.setActivo(null);
-		equipo.setIdEquipo(null);
-		equipo.setNombre("EPrueba");
-
-		FactoriaSA.getInstance().getSAEquipo().altaEquipo(equipo);
-
-		TTarea tarea = new TTarea();
-		tarea.setActivo(true);
-		tarea.setEquipo(null);
-		tarea.setIdTarea(null);
-		tarea.setNombre("aaaaaaaaaaaaaaaaaaaaaaaa");
-		tarea.setProducto(1);
-		tarea.setTerminada(false);
-
-		FactoriaSA.getInstance().getSATarea().altaTarea(tarea);
-
-		resultado = FactoriaSA.getInstance().getSAProducto().bajaProducto(1);
-		assertEquals("bajaProducto No Realizado (en tarea activa)", -3, resultado);
-	*/
-	}
-
+	
 }
